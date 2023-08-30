0bdcaa49935aed92f9fa89fc933f9b7857c28d66
moaaz <moaaz2tarik1@gmail.com>
2023-08-30 21:13:20 +0300

    added docstring and documentaion

--- app.py
+++ app.py
@@ -1,69 +1,89 @@
 import mediapipe as mp
-import cv2 as cv
-from hand_detector import Hand_Detector
+import cv2
+from hand_detector import HandDetector
 import time
 
-prev_frame_timer = 0
-new_frame_timer = 0
-model_path = r"C:\Users\moaaz\Desktop\wsl\hand_landmarker.task"
-test = Hand_Detector(model_path, 2, 0.5, 0.5, 0.5)
-cap = cv.VideoCapture(0)
-test.image_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-test.image_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-test.creat_zone()
+#######################
+# EXAMPLE DRIVER CODE #
+#######################
 
-try:
+if __name__ == "__main__":
+
+    # calculating fps
+    prev_frame_timer: float = 0
+    new_frame_timer: float = 0
+
+    # the model we are going to use
+    # link to the model
+    # https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task
+    model_path = r"hand_landmarker.task"
+
+    test = HandDetector(model_path, 2, 0.5, 0.5, 0.5 , bounding_box= True)
+    cap = cv2.VideoCapture(0)
+
+    # setting up we need to dimensions of our camera before we start
+    test.image_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    test.image_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # creating the track bars to set up the zone That we can move our finger in
+    test.movement_zone()
+
+    # font for the fps text
+    font = cv2.FONT_HERSHEY_SIMPLEX
     while True:
-
-        _, frame = cap.read()
-        frame = cv.flip(frame, 1)
-        font = cv.FONT_HERSHEY_SIMPLEX
         try:
 
+            # reading camera input
+            _, frame = cap.read()
+
+            # flipping the output image from the camera on the vertical axis
+            frame = cv2.flip(frame, 1)
+
+            # convert the camera feed to mp.Image format in order to be fed to the model
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-            res = test.landmarker.detect_for_video(mp_image, int(time.time() * 1_000))
+            # this is where the detection happens
+            res = test.detector.detect_for_video(mp_image, int(time.time() * 1_000))
+
+            # drawing the landmarks on the frame
             frame = test.draw_hand_landmarks(res, mp_image)
 
+            #  start recording the finger history of the movement of the finger
             if test.history_trigger:
-                test.record_fingertip_gesture([res.hand_landmarks[0][8].x, res.hand_landmarks[0][8].y],
-                                              [res.hand_landmarks[0][4].x, res.hand_landmarks[0][4].y],
-                                              [res.hand_landmarks[0][5].x, res.hand_landmarks[0][5].y])
+                test.history([res.hand_landmarks[0][8].x, res.hand_landmarks[0][8].y],
+                             [res.hand_landmarks[0][4].x, res.hand_landmarks[0][4].y],
+                             [res.hand_landmarks[0][5].x, res.hand_landmarks[0][5].y])
 
-            test.set_ratios()
-
-            if test.distance:
-                test.distance_of_gesture()
+            # start controlling the mouse
             if test.move_mouse:
                 test.move_cursor_with_finger()
-                test.look_for_clicks()
+                test.listen_for_clicks()
 
         except Exception as e:
             print(e)
 
-        key = cv.waitKey(1) & 0xFF
-        # print(key)
+        # listen to keyboard key
+        key = cv2.waitKey(1) & 0xFF
+
+        # q to close the app
         if key == ord('q'):
             cap.release()
-            cv.destroyAllWindows()
+            cv2.destroyAllWindows()
             break
 
+        # h to start recording the movement history
         elif key == ord("h"):
             test.history_trigger = not test.history_trigger
+
+        # p to enable mouse movement
         elif key == ord("p"):
             test.move_mouse = not test.move_mouse
 
+        # calculating and displaying the fps
         new_frame_timer = time.time()
         fps = (1 / (new_frame_timer - prev_frame_timer))
         fps = str(int(fps))
+        cv2.rectangle(frame, (test.x1, test.y1), (test.x2, test.y2), (0, 0, 255), 3)
+        cv2.putText(frame, str(fps), (10, 30), font, 1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
         prev_frame_timer = new_frame_timer
-        cv.rectangle(frame,
-                     (test.top_x, test.top_y),
-                     (test.bot_x, test.bot_y),
-                     (0, 0, 255),
-                     3)
-        cv.putText(frame, str(fps), (10, 30), font, 1, (0, 0, 0), 2)
-        cv.imshow('frame', frame)
-
-except Exception as e:
-    print(e)
--- hand_detector.py
+++ hand_detector.py
@@ -1,5 +1,6 @@
+from typing import Any
 import mediapipe as mp
-import cv2 as cv
+import cv2
 import numpy as np
 import mouse
 import tkinter as tk
@@ -12,34 +13,7 @@ HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-# imagew  imageh screnh screenw
-# 480.0   640.0  864    1536
-
-class Hand_Detector:
-    rightclick = False
-    right_click_trigger = False
-    smoothing_factor = 0.004
-    screen_width = 0
-    screen_height = 0
-    screen_dim = False
-    move_mouse = False
-    distance = False
-    # circular = np.loadtxt("gesture.txt")
-    # start recording gesture
-    history_trigger = False
-    # fingers history
-    finger0_tip_history = np.array([])
-    finger1_tip_history = np.array([[0, 0], [0, 0]])
-    finger2_tip_history = np.array([[0, 0], [0, 0]])
-    finger3_tip_history = np.array([[0, 0], [0, 0]])
-    finger4_tip_history = np.array([])
-    # image dimensions
-    image_width = 0
-    image_height = 0
-    top_x = image_width
-    top_y = image_height
-    bot_x = 0
-    bot_y = 0
+class HandDetector:
 
     def __init__(self, path,
                  num_hands=1,
@@ -47,13 +21,54 @@ class Hand_Detector:
                  presence_confidence=0.3,
                  tracking_confidence=0.3,
                  bounding_box=False):
+
+        # flag if right click is detected
+        self.right_click: bool = False
+
+        # trigger that will turn off the right click after being pressed once and wait
+        # for the next right click to avoid application freezing
+        self.right_click_trigger: bool = False
+
+        # factor for smoothing the mouse movement
+        self.smoothing_factor: int = 3
+
+        # pc screen dimensions
+        self.screen_width: int = 0
+        self.screen_height: int = 0
+
+        # turns on or off moving the mouse
+        self.move_mouse: bool = False
+
+        # start recording gesture
+        self.history_trigger: bool = False
+
+        # fingers history
+        self.finger1_tip_history: np.ndarray = np.array([[0, 0], [0, 0]])
+        self.finger2_tip_history: np.ndarray = np.array([[0, 0], [0, 0]])
+        self.finger3_tip_history: np.ndarray = np.array([[0, 0], [0, 0]])
+
+        # image dimensions
+        self.image_width: int = 0
+        self.image_height: int = 0
+
+        # zone dimensions for the finger movements
+        self.x1: int = 0
+        self.y1: int = 0
+        self.x2: int = 0
+        self.y2: int = 0
+
         # path to the model 
-        self.path = path
-        self.num_hands = num_hands
-        self.detection_confidence = detection_confidence
-        self.presence_confidence = presence_confidence
+        self.path: str = path
+
+        # number of hands to be detected
+        self.num_hands: int = num_hands
+
+        # confidence settings for the hand landmarks detector
+        self.detection_confidence: float = detection_confidence
+        self.presence_confidence: float = presence_confidence
+
         # base options 
-        self.options = HandLandmarkerOptions(
+        self.options: mp.tasks.vision.HandLandmarkerOptions = HandLandmarkerOptions(
             base_options=BaseOptions(model_asset_path=path),
             num_hands=num_hands,
             running_mode=VisionRunningMode.VIDEO,
@@ -62,82 +77,152 @@ class Hand_Detector:
             min_tracking_confidence=tracking_confidence
         )
         # making the detector 
-        self.landmarker = HandLandmarker.create_from_options(self.options)
+        self.detector = HandLandmarker.create_from_options(self.options)
 
         # get the bounding box   
         self.bounding_box = bounding_box
         root = tk.Tk()
-        self.screen_height = root.winfo_screenheight()
-        self.screen_width = root.winfo_screenwidth()
+        self.screen_height: int = root.winfo_screenheight()
+        self.screen_width: int = root.winfo_screenwidth()
         root.destroy()
 
-    def normalize(self, x: float, y: float, type: int) -> tuple[int, int]:
+    def normalize(self, x: float, y: float, norm_type: int | bool) -> tuple[int | Any, int | Any]:
         """
-        @param x: x coordinate from the landmark
-        @param y: y coordinate from the landmark
-        @param type : 1 -> denormalize ,  0-> normalize 
-        return:  x , y
+        Normalize or de-normalize the x and y coordinates based on the output image dimensions.
+        For normalization, x is divided by the width, and y is divided by the height.
+        For de-normalization, x is multiplied by the width, and y is multiplied by the height.
+
+        Parameters:
+        __________
+         x : float
+            The x-coordinate from the landmark.
+         y : float
+            The y-coordinate from the landmark.
+        norm_type : int | bool
+            1 for de-normalization, or 0 for normalization.
+
+        Returns:
+        _______
+        Normalized or de-normalized x and y coordinates as integers.
         """
-        if type == 1:
+
+        if norm_type == 1:
             return int(x * self.image_width), int(y * self.image_height)
-        elif type == 0:
+        elif norm_type == 0:
             return int(x / self.image_width), int(y / self.image_height)
         else:
             print("wrong value")
 
-    # bounding box of the hand 
-    def get_bounding_box(self, land_mark, img):
+    # bounding box of the hand
+    def get_bounding_box(self, land_marks: mp.tasks.vision.HandLandmarkerResult,
+                         offset: int = 10) -> tuple[int | Any, int | Any, int | Any, int | Any]:
+        """
+        Gets the bounding box around the hands with an option to apply an offset.
+
+        Parameters:
+        __________
+        land_marks : mp.tasks.vision.HandLandmarkerResult.hand_landmarks
+            The result object from the hand detector.
+        offset : int
+            The offset value to adjust the bounding box size.
+        land_marks : int
+            The result object from the hand detector.
+        Returns:
+        ________
+        tuple[int | Any, int | Any, int | Any, int | Any]
+             A tuple containing the modified bounding box coordinates (min_x, min_y, max_x, max_y).
+        """
 
         min_x, max_x, min_y, max_y = self.image_width, 0, self.image_height, 0
         for i in range(21):
-            x, y = self.normalize(land_mark[i].x, land_mark[i].y, 1)
+            x, y = self.normalize(land_marks[i].x, land_marks[i].y, 1)
             min_x = min(min_x, x)
             min_y = min(min_y, y)
             max_x = max(max_x, x)
             max_y = max(max_y, y)
 
-        # offset by 10 pixels
-        return min_x - 10, min_y - 10, max_x + 10, max_y + 10
+        # offset by 10 pixels:
+        return min_x - offset, min_y - offset, max_x + offset, max_y + offset
 
+    def draw_hand_landmarks(self, results: mp.tasks.vision.HandLandmarkerResult,
+                            img: mp.Image | np.ndarray) -> np.array:
+        """
 
-    def set_ratios(self):
+        Parameters:
+        __________
+        results: mp.tasks.vision.HandLandmarkerResult
+            The result object from the detector
+        img: mp.Image | np.ndarray
+             the image or frame we are going to draw the landmarks over
+        Returns:
+        ________
+        np.array:
+            The image with the landmarks in a numpy.ndarray format
+        """
 
-        self.ratioh = 300 / self.screen_height
-        self.ratiow = 300 / self.screen_width
-
-    def draw_hand_landmarks(self, results: any, img: np.array) -> np.array:
-
-        image = img.numpy_view()
+        image = img.numpy_view() if type(img) == mp.Image else img
 
         for i in range(len(results.handedness)):
-
             connections = mp.tasks.vision.HandLandmarksConnections.HAND_CONNECTIONS
 
             for mark in results.hand_landmarks:
-                for i in range(21):
-                    x, y = self.normalize(mark[i].x, mark[i].y, 1)
-                    cv.circle(image, (x, y), radius=8, color=(0, 0, 255), thickness=-1)
+                for g in range(21):
+                    x, y = self.normalize(mark[g].x, mark[g].y, 1)
+                    cv2.circle(image, (x, y), radius=8, color=(0, 0, 255), thickness=-1)
 
                 for j in connections:
                     x_start, y_start = self.normalize(mark[j.start].x, mark[j.start].y, 1)
                     x_end, y_end = self.normalize(mark[j.end].x, mark[j.end].y, 1)
 
-                    cv.line(image,
-                            (x_start, y_start),
-                            (x_end, y_end),
-                            (255, 0, 0),
-                            thickness=4)
+                    cv2.line(image,
+                             (x_start, y_start),
+                             (x_end, y_end),
+                             (255, 0, 0),
+                             thickness=4)
 
                 if self.bounding_box:
-                    min_x, min_y, max_x, max_y = self.get_bounding_box(mark, img)
-                    cv.rectangle(image,
-                                 (min_x, max_y),
-                                 (max_x, min_y),
-                                 (0, 255, 0),
-                                 1)
+                    min_x, min_y, max_x, max_y = self.get_bounding_box(mark)
+                    cv2.rectangle(image,
+                                  (min_x, max_y),
+                                  (max_x, min_y),
+                                  (0, 255, 0),
+                                  1)
         return image
 
-    def record_fingertip_gesture(self, first, second, third):
+    def history(self, first: np.ndarray | list[int],
+                second: np.ndarray | list[int],
+                third: np.ndarray | list[int]):
+        """
+        Record the history of movement for the 3 points from the hand landmarker.
+        The first point is used as the point that moves the mouse cursor.
+        If the second point touches the third point, it will trigger a left click.
+
+        Note:
+        _____
+            ONLY RECORDS THE LAST 2 FRAMES
+        Usage example:
+            - The first point can be the tip of the pointer finger (8).
+            - The second point can be the tip of the thumb (4).
+            - The third point can be the MCP of the pointer finger (5).
+
+        Example:
+        ________
+        >>>    self.history(
+        >>>        [result.hand_landmarks[0][8].x, result.hand_landmarks[0][8].y],
+        >>>        [result.hand_landmarks[0][4].x, result.hand_landmarks[0][4].y],
+        >>>        [result.hand_landmarks[0][5].x, result.hand_landmarks[0][5].y]
+        >>>    )
+
+        Parameter:
+        _________
+
+        first : np.ndarray | list[int]
+            Array that contains the x, y coordinates of the first point.
+        second : np.ndarray | list[int]
+            Array that contains the x, y coordinates of the second point.
+        third : np.ndarray | list[int]
+            Array that contains the x, y coordinates of the third point.
+        """
 
         if self.finger1_tip_history.shape[0] <= 1:
             self.finger1_tip_history = np.append(self.finger1_tip_history, [first],
@@ -159,35 +244,57 @@ class Hand_Detector:
             self.finger3_tip_history = np.append(self.finger3_tip_history, [third],
                                                  axis=0)
 
-    def get_distance(self, normalized_coordinates):
+    # enable mouse clicks. for now, it's only left click.
+    def listen_for_clicks(self, left_click_threshold: float = 0.009, left_click_listener_threshold: float = 0.08):
+        """
+        Detects left-click actions based on finger distances using provided threshold values.
 
-        difference = np.array([[normalized_coordinates[1][0] - normalized_coordinates[0][0],
-                                normalized_coordinates[1][1] - normalized_coordinates[0][1]]])
+        This function calculates distances between normalized coordinates using `np.linalg.norm()`.
 
-        return difference * np.array([self.screen_width, self.screen_height])
+        A left click is triggered when the distance between the second and third points is <= to the
+        `left_click_threshold`. The function will avoid further left clicks until the distance between the
+        second and third points is greater >= to the `left_click_listener_threshold`.
 
-    def look_for_clicks(self):
+        Parameters
+        _________
+        left_click_threshold : float
+            The threshold distance for left-click activation.
+        left_click_listener_threshold : float
+            The threshold distance for reactivating left-click detection.
+        """
 
         finger1_finger2_dist = np.linalg.norm(self.finger3_tip_history[1] -
                                               self.finger2_tip_history[1])
 
-        if finger1_finger2_dist < 0.009:
-            self.rightclick = True
-        if finger1_finger2_dist > 0.08:
+        if finger1_finger2_dist <= left_click_threshold:
+            self.right_click = True
+        if finger1_finger2_dist > left_click_listener_threshold:
             self.right_click_trigger = False
             print("now listening to clicks ")
 
     def move_cursor_with_finger(self):
+        """
+        this function enables moving the cursor relative the position of the finger and also responsible for the
+        execution of the clicks.
 
+        Examples
+        --------
+
+        >>>    key = cv2.waitKey(1) & 0xFF
+        >>>
+        >>>    if key == ord("p"):
+        >>>       self.move_mouse = not self.move_mouse
+
+        """
         x_1, y_1 = self.normalize(self.finger1_tip_history[1][0],
                                   self.finger1_tip_history[1][1], 1)
 
-        x_1 = np.interp(x_1, (min(self.top_x, self.bot_x),
-                              max(self.top_x, self.bot_x)),
+        x_1 = np.interp(x_1, (min(self.x1, self.x2),
+                              max(self.x1, self.x2)),
                         (0, self.screen_width))
 
-        y_1 = np.interp(y_1, (min(self.top_y, self.bot_y),
-                              max(self.top_y, self.bot_y)),
+        y_1 = np.interp(y_1, (min(self.y1, self.y2),
+                              max(self.y1, self.y2)),
                         (0, self.screen_height))
 
         curr_x, curr_y = mouse.get_position()
@@ -196,28 +303,47 @@ class Hand_Detector:
 
         mouse.move(x_1, y_1, absolute=True)
 
-        if self.right_click_trigger is False and self.rightclick is True:
-            self.rightclick = False
+        if self.right_click_trigger is False and self.right_click is True:
+            self.right_click = False
             self.right_click_trigger = True
             mouse.click()
             print("pressed left click")
 
-    def _change_zone(self, x):
-        if cv.getWindowProperty("zone", cv.WND_PROP_VISIBLE) < 1:
-
+    # x returns the new value of the track bar when it is changed, but doesn't specify which bar has changed,
+    # so we don't use it.
+    def _change_zone(self, x=None):
+        """
+        the callback function for the track bars. this function is responsible for updating the movement_zone points and
+        the smoothing factor. x is required by cv2.getTrackbarPos, but it won't be used.
+        """
+        if cv2.getWindowProperty("zone", cv2.WND_PROP_VISIBLE) < 1:
             print("closed track bar")
-
         else:
+            self.x1 = int((cv2.getTrackbarPos("x1", "zone") / 100) * self.image_width)
+            self.y1 = int((cv2.getTrackbarPos("y1", "zone") / 100) * self.image_height)
+            self.x2 = int((cv2.getTrackbarPos("x2", "zone") / 100) * self.image_width)
+            self.y2 = int((cv2.getTrackbarPos("y2", "zone") / 100) * self.image_height)
+            self.smoothing_factor = cv2.getTrackbarPos("smoothing", "zone")
 
-            self.top_x = int((cv.getTrackbarPos("top_x", "zone") / 100) * self.image_width)
-            self.top_y = int((cv.getTrackbarPos("top_y", "zone") / 100) * self.image_height)
-            self.bot_x = int((cv.getTrackbarPos("bot_x", "zone") / 100) * self.image_width)
-            self.bot_y = int((cv.getTrackbarPos("bot_y", "zone") / 100) * self.image_height)
+    def movement_zone(self):
+        """
+        creates 5 track bars for making a mini zone that will move the cursor relative to the position of the finger.
+        this function is made because the model will lose the tracking of the hand if a part of the hand disappears
+        making it impossible to reach the bottom and the sides of the screen. this zone makes it easier to reach the
+        desired point.
 
-    def creat_zone(self):
-        cv.namedWindow("zone")
-        cv.resizeWindow("zone", 400, 180)
-        cv.createTrackbar("top_x", "zone", 0, 100, self._change_zone)
-        cv.createTrackbar("top_y", "zone", 0, 100, self._change_zone)
-        cv.createTrackbar("bot_x", "zone", 0, 100, self._change_zone)
-        cv.createTrackbar("bot_y", "zone", 0, 100, self._change_zone)
+        - x1: x-axis of the first point
+        - y1: y-axis of the first point
+        - x2: x-axis of the second point
+        - y2: y-axis of the second point
+        - smoothing: the movement smoothing factor. This is made because the inaccuracies of the model make it
+          impossible for the cursor to stay still.
+        """
+        cv2.namedWindow("zone")
+        cv2.resizeWindow("zone", 400, 300)
+        cv2.createTrackbar("x1", "zone", 0, 100, self._change_zone )
+        cv2.createTrackbar("y1", "zone", 0, 100, self._change_zone)
+        cv2.createTrackbar("x2", "zone", 0, 100, self._change_zone)
+        cv2.createTrackbar("y2", "zone", 0, 100, self._change_zone)
+        # higher is slower cursor movement
+        cv2.createTrackbar("smoothing", "zone", 0, 100, self._change_zone)
